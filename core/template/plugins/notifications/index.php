cca644a30793b5528c0fa092af666a2c7bc4071e
rasclatt <rasclatt@me.com>
2019-12-26 09:29:24 -0800

    Update notification decryption

--- core/template/plugins/notifications/index.php
+++ core/template/plugins/notifications/index.php
@@ -13,9 +13,16 @@ $msg		=	(!empty($this->getRequest('msg')))? $this->getRequest('msg') : false;
 		<div class="nbr_success pointer"><?php echo implode('</div><div class="nbr_success">', $success) ?></div>
 	</div>
 <?php elseif(!empty($msg)): ?>
+
 	<div class="col-2">
-		<div class="nbr_warning pointer"><?php echo $msg ?></div>
+		<div class="nbr_warning pointer<?php if($delay == 'off') echo ' stay' ?>"><?php
+			$dec = $this->getHelper('nCrypt')->decOpenSSL(urldecode($msg));
+			 if(empty($dec))
+			 	echo (is_numeric($msg))? \Nubersoft\ErrorMessaging::getMessage($msg) : $msg;
+			 else
+				echo $dec; ?><span class="close-x">&times;</span></div>
 	</div>
+
 <?php endif ?>
 <script>
 	$(function(){
