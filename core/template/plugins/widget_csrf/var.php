4d872baecbc39247b754b0d9dcd3e80ea2d5364b
Rasclatt <rasclatt@me.com>
2021-02-08 11:47:41 -0800

    Update JWT Builder

--- core/template/plugins/widget_csrf/var.php
+++ core/template/plugins/widget_csrf/var.php
@@ -3,9 +3,10 @@ use \Nubersoft\ {
     JWTFactory as JWT,
     JWT\Controller
 };
-
-if(!Controller::getJwtTokenSecret())
-    return false
+# Create a jwttoken if not already done
+if(!Controller::getJwtTokenSecret()) {
+    file_put_contents(NBR_CLIENT_DIR.DS.'settings'.DS.'.jwttoken', md5(rand()));
+}
 ?><script>
 var csrf    =   "<?php echo JWT::get()->create([
     'csrf' => md5(time()),
