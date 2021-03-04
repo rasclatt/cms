1e210bcc45a7ffeeec93552eeda0e901cd23567f
rasclatt <rasclatt@me.com>
2021-03-04 14:31:29 -0800

    Update Service
    Allow translations service

--- domain/index.php
+++ domain/index.php
@@ -15,6 +15,31 @@ if(!empty($argv[1])) {
  *	@description	Loader assist functions
  */
 if(!function_exists('Logit')) {
+    function jwtChecker($nApp)
+    {
+        # Run jwt checks if there is a checker
+        if(class_exists('\Nubersoft\JWT\Controller')) {
+            # Fetch token secret
+            $jwt = JWT\Controller::getJwtTokenSecret();
+            # If there is a secret
+            if($jwt) {
+                if(!empty($nApp->getPost())) {
+                    try {
+                        # Validate the token
+                        JWTFactory::get()->get($nApp->dec($nApp->getPost('jwtToken')));
+                    }
+                    catch (\Exception $e) {
+                        //throw new Exception\Ajax($nApp->getPost('jwtToken'), 403);
+                        # Report back with ajax or same-page
+                        if($nApp->isAjaxRequest())
+                            throw new Exception\Ajax('Invalid security token', 403);
+                        # Throw normal exception
+                        throw new \Exception('Invalid security token', 403);
+                    }
+                }
+            }
+        }
+    }
     # Save basic logging of errors
     function Logit($e)
     {
@@ -83,27 +108,11 @@ try {
                 # Hide errors
                 ini_set('display_errors', 0);
             }
-            # Run jwt checks if there is a checker
-            if(class_exists('\Nubersoft\JWT\Controller')) {
-                # Fetch token secret
-                $jwt = JWT\Controller::getJwtTokenSecret();
-                # If there is a secret
-                if($jwt) {
-                    if(!empty($nApp->getPost())) {
-                        try {
-                            # Validate the token
-                            JWTFactory::get()->get($nApp->dec($nApp->getPost('jwtToken')));
-                        }
-                        catch (\Exception $e) {
-                            //throw new Exception\Ajax($nApp->getPost('jwtToken'), 403);
-                            # Report back with ajax or same-page
-                            if($nApp->isAjaxRequest())
-                                throw new Exception\Ajax('Invalid security token', 403);
-                            # Throw normal exception
-                            throw new \Exception('Invalid security token', 403);
-                        }
-                    }
-                }
+            # Allow translation to get through
+            if($nApp->getRequest('service') != 'translation') {
+                # Check if action is run
+                if(!empty($nApp->getRequest('action')))
+                    jwtChecker($nApp);
             }
             # Start our program
             $AutomatorController->createWorkflow('default');
