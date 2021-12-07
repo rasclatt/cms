38cd8bfac0154a6735308bb7e08fb3bb530d0058
rasclatt <rasclatt@me.com>
2021-12-07 09:00:27 -0700

    Admin table check

--- core/template/default/backend/table.php
+++ core/template/default/backend/table.php
@@ -1,7 +1,17 @@
 <?php
+use \Nubersoft\nQuery;
 # Stop if not admin
 if (!$this->isAdmin())
 	return false;
+# Store the table
 $table = $this->getRequest('table');
+# Get the tables from db
+$tablesAllowed = array_map(function($v) {
+	return $v[key($v)];
+}, (new nQuery)->query("SHOW TABLES")->getResults());
+# If not allowed, stop
+if(!in_array($table, $tablesAllowed))
+	throw new \Exception('Invalid request.');
+# Fetch table
 $use = ($this->getGet('subaction') == 'interface') ? "admintools" : "table_view";
 echo $this->getPlugin($use, ($this->getGet('subaction') == 'interface') ? DS . $table . DS . 'index.php' : '');
