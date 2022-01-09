a72cb4de9e149c71479e10cef677b68f74694e9a
Rasclatt <rasclatt@me.com>
2022-01-09 00:03:41 -0800

    Message on RSA not readable

--- core/template/plugins/widget_ftp/index.php
+++ core/template/plugins/widget_ftp/index.php
@@ -2,7 +2,7 @@
 use \phpseclib3\Crypt\PublicKeyLoader as RSA;
 use \phpseclib3\Net\SFTP;
 use \Widget\WidgetFtp;
-use \Nubersoft\ {
+use \Nubersoft\{
     JWTFactory,
     nForm as Form,
     Settings
@@ -10,11 +10,11 @@ use \Nubersoft\ {
 
 $plugin_id = basename(__DIR__);
 
-if($this->getPost('action') == 'widget_admin_ftp_settings') {
-    foreach($this->getPost('settings') as $k => $v) {
+if ($this->getPost('action') == 'widget_admin_ftp_settings') {
+    foreach ($this->getPost('settings') as $k => $v) {
         (new Settings)->deleteOption($k, 'system')->setOption($k, $v, 'system');
     }
-    $this->redirect('?load='.$plugin_id);
+    $this->redirect('?load=' . $plugin_id);
 }
 
 spl_autoload_register(function ($class) {
@@ -41,43 +41,43 @@ define('REMOTE_FTPSIP', $form->remoteftpip);
         <?php if (empty($form->remoteftpip)) : ?>
             category_id = remoteftpip | option_attribute = {{the ip of your server}}<br />
         <?php endif ?>
-        
+
         <?php if (empty($form->remoteftproot)) : ?>
             category_id = remoteftproot | option_attribute = {{the server root path to your domain root}}<br />
         <?php endif ?>
-        
+
         <?php if (empty($form->remoteftppriv)) : ?>
             category_id = remoteftppriv | option_attribute = {{the local path to the private key}}<br />
         <?php endif ?>
-        
+
         <?php if (empty($form->remoteftpuri)) : ?>
             category_id = remoteftpuri | option_attribute = {{https://www.example.com}}<br />
         <?php endif ?>
-        
+
         <?php if (empty($form->remoteftpuser)) : ?>
             category_id = remoteftpuser | option_attribute = {{username}}<br />
         <?php endif ?>
 
     </code>
 
-    <?php return false;
+<?php return false;
 endif;
 ?>
-    <?php echo Form::getOpen([ 'action' => '#', 'enctype' => 'multipart/form-data' ]) ?>
-        <?php echo Form::getFullhide([ 'name' => 'action', 'value' => 'widget_admin_ftp_settings' ]) ?>
-        
-        <?php echo Form::getText([ 'label' => 'Remote Ip', 'name' => 'settings[remoteftpip]', 'value' => $form->remoteftpip, 'class' => 'nbr' ]) ?>
-        
-        <?php echo Form::getText([ 'label' => 'Remote FTP Root', 'name' => 'settings[remoteftproot]', 'value' => $form->remoteftproot, 'class' => 'nbr' ]) ?>
-        
-        <?php echo Form::getText([ 'label' => 'Private Key', 'name' => 'settings[remoteftppriv]', 'value' => $form->remoteftppriv, 'class' => 'nbr' ]) ?>
-        
-        <?php echo Form::getText([ 'label' => 'Remote Username', 'name' => 'settings[remoteftpuser]', 'value' => $form->remoteftpuser, 'class' => 'nbr' ]) ?>
-        
-        <?php echo Form::getText([ 'label' => 'Remote URI', 'name' => 'settings[remoteftpuri]', 'value' => $form->remoteftpuri, 'class' => 'nbr' ]) ?>
-        
-        <?php echo Form::getSubmit([ 'value' => 'Save', 'class' => 'button standard' ]) ?>
-    <?php echo Form::getClose() ?>
+<?php echo Form::getOpen(['action' => '#', 'enctype' => 'multipart/form-data']) ?>
+<?php echo Form::getFullhide(['name' => 'action', 'value' => 'widget_admin_ftp_settings']) ?>
+
+<?php echo Form::getText(['label' => 'Remote Ip', 'name' => 'settings[remoteftpip]', 'value' => $form->remoteftpip, 'class' => 'nbr']) ?>
+
+<?php echo Form::getText(['label' => 'Remote FTP Root', 'name' => 'settings[remoteftproot]', 'value' => $form->remoteftproot, 'class' => 'nbr']) ?>
+
+<?php echo Form::getText(['label' => 'Private Key', 'name' => 'settings[remoteftppriv]', 'value' => $form->remoteftppriv, 'class' => 'nbr']) ?>
+
+<?php echo Form::getText(['label' => 'Remote Username', 'name' => 'settings[remoteftpuser]', 'value' => $form->remoteftpuser, 'class' => 'nbr']) ?>
+
+<?php echo Form::getText(['label' => 'Remote URI', 'name' => 'settings[remoteftpuri]', 'value' => $form->remoteftpuri, 'class' => 'nbr']) ?>
+
+<?php echo Form::getSubmit(['value' => 'Save', 'class' => 'button standard']) ?>
+<?php echo Form::getClose() ?>
 
 <?php # If the copy request is made
 if ($this->getPost('action') == 'copyftp') {
@@ -89,7 +89,7 @@ if ($this->getPost('action') == 'copyftp') {
                 'method' => 'POST',
                 'header' => [
                     'Content-Type: application/x-www-form-urlencoded',
-                    'Authorization: Bearer '.\NubersoftCms\Model\Api::generateKey()
+                    'Authorization: Bearer ' . \NubersoftCms\Model\Api::generateKey()
                 ],
                 'content' => [
                     'service' => 'Admin.generateKey'
@@ -111,6 +111,8 @@ if ($this->getPost('action') == 'copyftp') {
     } else {
         $key = $form->remoteftppriv;
         $sftp = new SFTP(REMOTE_FTPSIP);
+        if (is_file($key) && !is_readable($key))
+            throw new Exception('The key file is unreadable. Requires a permission modification.');
         $key = RSA::load(file_get_contents($key));
         if (!$sftp->login($Settings->getSettings()->remoteftpuser, $key)) {
             throw new Exception('Login failed');
@@ -118,10 +120,10 @@ if ($this->getPost('action') == 'copyftp') {
         $pg = str_replace('//', '/', SFTP_SERVER_ROOT . '/' . ltrim($page, '/'));
         $Ftp = new \Widget\Ftp($sftp, $pg);
         $response = $Ftp
-        ->addIgnore('path', '/vendor')
-        ->addIgnore('path', '/domain/core')
-        ->addIgnore('path', '/domain/phpMyAdmin')->fetch()->toLocal(NBR_ROOT_DIR, $this);
-        echo '<code>'.printpre($response, 1).'</code>';
+            ->addIgnore('path', '/vendor')
+            ->addIgnore('path', '/domain/core')
+            ->addIgnore('path', '/domain/phpMyAdmin')->fetch()->toLocal(NBR_ROOT_DIR, $this);
+        echo '<code>' . printpre($response, 1) . '</code>';
     }
 }
 ?>
@@ -132,26 +134,26 @@ if ($this->getPost('action') == 'copyftp') {
 </div>
 
 <?php echo Form::getOpen(['action' => '?load=widget_ftp']) ?>
-    <?php echo Form::getFullhide(['name' => 'action', 'value' => 'copyftp', 'class' => 'nbr']) ?>
-    <div class="col-count-4 gapped col-c2-lg col-c1-md">
-        <?php echo Form::getSelect(['label' => 'Select Subpage to Copy', 'name' => 'dir', 'class' => 'nbr', 'options' => [
-            [
-                'name' => 'Database Import',
-                'value' => 'dump'
-            ],
-            [
-                'name' => 'Domain Root (Frontend)',
-                'value' => 'domain'
-            ],
-            [
-                'name' => 'Client Folder (Backend)',
-                'value' => 'client'
-            ],
-            [
-                'name' => 'Root',
-                'value' => ''
-            ]
-        ]]) ?>
+<?php echo Form::getFullhide(['name' => 'action', 'value' => 'copyftp', 'class' => 'nbr']) ?>
+<div class="col-count-4 gapped col-c2-lg col-c1-md">
+    <?php echo Form::getSelect(['label' => 'Select Subpage to Copy', 'name' => 'dir', 'class' => 'nbr', 'options' => [
+        [
+            'name' => 'Database Import',
+            'value' => 'dump'
+        ],
+        [
+            'name' => 'Domain Root (Frontend)',
+            'value' => 'domain'
+        ],
+        [
+            'name' => 'Client Folder (Backend)',
+            'value' => 'client'
+        ],
+        [
+            'name' => 'Root',
+            'value' => ''
+        ]
+    ]]) ?>
 
     <div class="span4 span2-lg span1-md">
         <?php echo Form::getSubmit(['value' => 'Save', 'class' => 'nbr auto']) ?>
