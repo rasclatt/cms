0636c64a4203f7431d2486bd9f141007d627f983
Rasclatt <rasclatt@me.com>
2022-03-03 20:21:26 -0800

    Core Updates

--- core/template/plugins/notifications/index.php
+++ core/template/plugins/notifications/index.php
@@ -1,52 +1,57 @@
 <?php
-use \Nubersoft\ {
+use \Nubersoft\{
     JWTFactory as JWT
 };
-$JWT = JWT::get();
-$success = array_unique($this->getSystemMessages('success'));
-$errors = array_unique($this->getSystemMessages('errors'));
-$msg = (!empty($this->getRequest('msg')))? $this->getRequest('msg') : false;
 
+$JWT = JWT::get();
+$toMessages = function ($array) {
+    if (!empty($array)) {
+        $new = [];
+        \Nubersoft\ArrayWorks::extractAll($array, $new);
+        return $new;
+    }
+    return $array;
+};
+$success = $toMessages(array_unique($this->getSystemMessages('success')));
+$errors = $toMessages(array_unique($this->getSystemMessages('errors')));
+$msg = (!empty($this->getRequest('msg'))) ? $this->getRequest('msg') : false;
 try {
     $data   =   $JWT->get($msg);
-    $expire    =   ($data['expire'])?? false;
-    if($expire) {
+    $expire    =   ($data['expire']) ?? false;
+    if ($expire) {
         $expired    =   ($expire < time());
-        $msg    =   ($data['msg'])?? false;
+        $msg    =   ($data['msg']) ?? false;
     }
+} catch (\Exception $e) {
 }
-catch (\Exception $e) {
-    
-}
-
 $hasAlert   =   (!empty($errors) || !empty($success));
 ?>
 
-<?php if($hasAlert): ?>
-<div class="start2 align-middle alert-wrapper">
-    <span class="alert-dismissible">
+<?php if ($hasAlert) : ?>
+    <div class="start2 align-middle alert-wrapper">
+        <span class="alert-dismissible">
+        <?php endif ?>
+
+        <?php if (!empty($errors)) : ?>
+            <div class="alert alert-danger pointer dismiss-parent" role="alert"><?php echo implode('</div><div class="alert alert-danger pointer" role="alert">', $errors) ?></div>
+        <?php elseif (!empty($success)) : ?>
+            <div class="alert alert-success pointer dismiss-parent" role="alert"><?php echo implode('</div><div class="alert alert-success pointer" role="alert">', $success) ?></div>
+        <?php endif ?>
+
+        <?php if ($hasAlert) : ?>
+        </span>
+    </div>
 <?php endif ?>
 
-    <?php if(!empty($errors)): ?>
-    <div class="alert alert-danger pointer dismiss-parent" role="alert"><?php echo implode('</div><div class="alert alert-danger pointer" role="alert">', $errors) ?></div>
-    <?php elseif(!empty($success)): ?>
-    <div class="alert alert-success pointer dismiss-parent" role="alert"><?php echo implode('</div><div class="alert alert-success pointer" role="alert">', $success) ?></div>
-    <?php endif ?>
+<?php if (isset($expired)) : ?>
+    <?php if (!$expired) : ?>
 
-<?php if($hasAlert): ?>
-    </span>
-</div>
-<?php endif ?>
-
-<?php if(isset($expired)): ?>
-    <?php if(!$expired): ?>
-
-	<div class="start2 align-middle alert-wrapper">
-		<div class="alert alert-warning alert-dismissible fade show pointer<?php if(!empty($delay) && ($delay == 'off')) echo ' stay' ?>" role="alert"><?php
-            echo $this->getHelper('ErrorMessaging')->getMessageAuto($msg);
-            ?><button class="close-x close" data-dismiss="alert" aria-label="Close">&times;</button>
+        <div class="start2 align-middle alert-wrapper">
+            <div class="alert alert-warning alert-dismissible fade show pointer<?php if (!empty($delay) && ($delay == 'off')) echo ' stay' ?>" role="alert"><?php
+                                                                                                                                                            echo $this->getHelper('ErrorMessaging')->getMessageAuto($msg);
+                                                                                                                                                            ?><button class="close-x close" data-dismiss="alert" aria-label="Close">&times;</button>
+            </div>
         </div>
-	</div>
 
-    <?php endif ?>    
+    <?php endif ?>
 <?php endif ?>
